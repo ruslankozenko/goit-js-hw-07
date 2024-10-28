3d7f85602888583461fe3e2aa3d8887f4b68fe9e
Ruslan Kozenko <kozenko.r@seznam.cz>
2024-10-28 18:58:12 +0100

    task1 off

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach(item => {
+    const title = item.querySelector('h2').textContent; 
+    const elementsCount = item.querySelectorAll('li').length;
+    
+    console.log(`Category: ${title}`);
+    
+    console.log(`Elements: ${elementsCount}`);
+});
+
