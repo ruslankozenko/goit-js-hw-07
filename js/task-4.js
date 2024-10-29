71f5dba171794eeb4badf5126c5a7b1a38bf4e6f
Ruslan Kozenko <kozenko.r@seznam.cz>
2024-10-29 16:55:06 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleForm);
+
+function handleForm(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    return alert("All form fields must be filled in");
+  }
+  const info = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  event.target.reset();
+  console.log(info);
+}
